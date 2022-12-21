b4eac725d13a69d952725fe46f7408407fb75d1b
Lea <lea.il@hotmail.com>
2022-12-21 12:34:43 +0100

    Added "noopener" to link in source

--- src/Source.js
+++ src/Source.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./Source.css";
 
 export default function Source() {
-  return <footer> <a href="https://github.com/leairene/weather-react" target="_blank" rel="noreferrer">Open sourced code</a> by Lea Irene Larssen</footer>;
+  return (
+    <footer>
+      {" "}
+      <a
+        href="https://github.com/leairene/weather-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open sourced code
+      </a>{" "}
+      by Lea Irene Larssen
+    </footer>
+  );
 }
