54201fceefeac0217f0e49c39dd2e44a79747745
Lea <lea.il@hotmail.com>
2022-12-21 12:33:44 +0100

    Added open source link to footer

--- src/Source.js
+++ src/Source.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Source.css";
 
 export default function Source() {
-  return <footer>Coded by Lea Irene Larssen</footer>;
+  return <footer> <a href="https://github.com/leairene/weather-react" target="_blank" rel="noreferrer">Open sourced code</a> by Lea Irene Larssen</footer>;
 }
